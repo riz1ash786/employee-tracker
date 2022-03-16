39439bb9c4092891ae073f3e52b96c41e871c7c5
Rizwan Ashraf <riz1ash786@gmail.com>
2022-03-16 14:04:35 +0000

    empoyee details in seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,9 +11,19 @@ VALUES
 INSERT INTO role (title, salary, department_id)
 VALUES
 ('Web Developer', 90000, 1),
-('Senior Accountant', 110000, 2),
+('Financial Controller', 110000, 2),
 ('Paralegal', 60000, 3),
 ('Sales Manager', 70000, 4),
 ('HR Manager', 50000, 5),
 ('Software Engineer', 120000, 1),
-('Finance Controller', 70000, 2);
+('Finance Controller', 70000, 2),
+('Compliance Manager', 100000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('Rizwan', 'Ashraf', 1, 86),
+('Lewis', 'Hmailton', 2, 44),
+('Cristiano', 'Ronaldo', 3, 07),
+('Imran', 'Khan', 4, 17),
+('Mike', 'Tyson', 5, 50),
+('Michael', 'Jordan', 6, 23);
